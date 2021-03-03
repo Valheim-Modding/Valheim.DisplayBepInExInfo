7ee28bbb4498fbcf6ff2d5b865cd80b3d638aa6a
ghorsington <neigh@coder.horse>
2021-03-03 23:37:26 +0200

    Initial commit

---
+++ Valheim.DisplayBepInExInfo/DisplayInfoPlugin.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using BepInEx;
+using BepInEx.Bootstrap;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using HarmonyLib;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Valheim.DisplayBepInExInfo
+{
+    [BepInPlugin("org.bepinex.valheim.displayinfo", "Display BepInEx Info In-Game", "1.0.0")]
+    public class DisplayInfoPlugin : BaseUnityPlugin
+    {
+        internal static ConfigEntry<LogLevel> LogLevels;
+        internal static ConfigEntry<bool> DisplayLogsInConsole;
+
+        internal static string BepInExVersion => typeof(BaseUnityPlugin).Assembly.GetName().Version.ToString();
+
+        private void Awake()
+        {
+            LogLevels = Config.Bind("Console", "LogLevels",
+                LogLevel.Message | LogLevel.Info | LogLevel.Warning | LogLevel.Error,
+                "Log levels to log to Valheim console.");
+            DisplayLogsInConsole = Config.Bind("Console", "DisplayBepInExLogs",
+                true,
+                "If true, will display BepInEx logs in Valheim console.");
+
+            if (DisplayLogsInConsole.Value)
+                BepInEx.Logging.Logger.Listeners.Add(new ValheimConsoleListener());
+
+            Harmony.CreateAndPatchAll(typeof(DisplayInfoPlugin));
+        }
+
+        [HarmonyPatch(typeof(FejdStartup), "Start")]
+        [HarmonyPostfix]
+        private static void OnFejdStartup(FejdStartup __instance)
+        {
+            var parent = __instance.m_versionLabel.transform.parent.gameObject;
+            var bepinGo = new GameObject("BepInEx Version");
+            bepinGo.transform.parent = parent.transform;
+            bepinGo.AddComponent<CanvasRenderer>();
+            bepinGo.transform.localPosition = Vector3.zero;
+            var rt = bepinGo.AddComponent<RectTransform>();
+            rt.anchorMin = new Vector2(0.03f, 0.95f);
+            rt.anchorMax = new Vector2(0.3f, 0.95f);
+            var text = bepinGo.AddComponent<Text>();
+            text.font = Font.CreateDynamicFontFromOSFont("Arial", 20);
+            text.text =
+                $"Running BepInEx {BepInExVersion}\n{Chainloader.PluginInfos.Count} plugins loaded\nPress F5 to open console";
+            text.color = Color.white;
+            text.fontSize = 20;
+        }
+
+        [HarmonyPatch(typeof(ZNet), nameof(ZNet.SetServer))]
+        [HarmonyPostfix]
+        private static void OnServerStart(string serverName)
+        {
+            var cm = typeof(BaseUnityPlugin).Assembly.GetType("BepInEx.ConsoleManager", false);
+            if (cm == null)
+                return;
+            var setTitle = AccessTools.MethodDelegate<Action<string>>(AccessTools.Method(cm, "SetConsoleTitle"));
+            setTitle($"BepInEx {BepInExVersion} - Valheim Server - {serverName}");
+        }
+    }
+}
---
+++ Valheim.DisplayBepInExInfo/ValheimConsoleListener.cs
@@ -0,0 +1,19 @@
+﻿using BepInEx.Logging;
+
+namespace Valheim.DisplayBepInExInfo
+{
+    internal class ValheimConsoleListener : ILogListener
+    {
+        public void Dispose()
+        {
+        }
+
+        public void LogEvent(object sender, LogEventArgs eventArgs)
+        {
+            if ((eventArgs.Level & DisplayInfoPlugin.LogLevels.Value) == 0)
+                return;
+            if (Console.instance)
+                Console.instance.Print(eventArgs.ToString());
+        }
+    }
+}
